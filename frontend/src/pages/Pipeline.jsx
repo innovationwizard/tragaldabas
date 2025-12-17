cfb05a6d7dac590956925cf24ffaad3c8492ef34
Jorge Luis Contreras Herrera <jorgeluiscontrerasherrera@gmail.com>
2025-12-16 23:50:07 -0600

    Add missing supabase import to Pipeline.jsx

--- frontend/src/pages/Pipeline.jsx
+++ frontend/src/pages/Pipeline.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import axios from 'axios'
+import { supabase } from '../lib/supabase'
 import Layout from '../components/Layout'
 
 const STAGES = [
